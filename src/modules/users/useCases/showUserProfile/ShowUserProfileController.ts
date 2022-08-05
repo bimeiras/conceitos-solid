0c8a1a0994e157f5bfba78c30bc311115017a122
bimeiras <mbmeira31@gmail.com>
2022-08-05 11:55:59 -0300

    desafio finalizado e testado

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,14 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const foundUser = this.showUserProfileUseCase.execute({ user_id });
+
+      return response.status(200).json(foundUser);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
